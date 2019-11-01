8a8d81c7f974a5b1ab19ab8e194a35ee51f84c52
ZIN MYO <overmidnight12@gmail.com>
2019-11-01 16:56:57 -0700

    my changes

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RecipeDetails extends Component {
+    render() {
+        return (
+            <React.Fragment>Hello From Details</React.Fragment>
+        )
+    }
+}
