5225728e82fc5a3635b8292fe04f070a296551d0
ZIN MYO <overmidnight12@gmail.com>
2019-11-02 15:22:11 -0700

    my changes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -15,7 +15,17 @@ export default class Recipe extends Component {
                 <h1>Recipe</h1>
                 <div className="col-10 mx-auto col-md-6 col-lg-4">
                     <div className="card">
-
+                        <img src={image_url} className="img-card-top" style={{height:"14rem"}} alt="recipe" />
+                        <div className="card-body text-capitalize">
+                            <h5>{title}</h5>
+                            <h6 className="text-warning text-slanted">
+                                provided by {publisher}
+                            </h6>
+                        </div>
+                        <div className="card-footer">
+                            <button type="button" className="btn btn-outline-primary">Details</button>
+                            <a href="{source_url}" className="btn btn-outline-dark mx-2">Recipe url</a>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
