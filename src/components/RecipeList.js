afc529848fe6f1d362836fe56f622ed8f042c3f7
ZIN MYO <overmidnight12@gmail.com>
2019-11-01 17:04:41 -0700

    my changes

---
+++ src/components/RecipeList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RecipeList extends Component {
+    render() {
+        return (
+            <React.Fragment>Recipe List</React.Fragment>
+        )
+    }
+}
---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RecipeSearch extends Component {
+    render() {
+        return (
+            <React.Fragment>Recipe Search</React.Fragment>
+        )
+    }
+}
