c3cd7431748c156ef08f790de8b3a6485f0b6e42
ZIN MYO <overmidnight12@gmail.com>
2019-11-01 17:50:48 -0700

    my changes

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Recipe from './Recipe';
+import RecipeSearch from './RecipeSearch';
 
 export default class RecipeList extends Component {
     render() {
         return (
             <React.Fragment>
                 <h1>Recipe List</h1>
+                <Recipe />
+                <RecipeSearch />
             </React.Fragment>
         )
     }
